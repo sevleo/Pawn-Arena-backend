a3c5480999b9b2ecad3243ba98c69a3de9529cc3
Seva <seva.leonov@hotmail.com>
2024-07-15 20:20:01 +0700

    Add socketio connection

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const Express = require("express")();
 const Http = require("http").Server(Express);
-const Socketio = require("socket.io")(Http);
+// const Socketio = require("socket.io")(Http);
+const Socketio = require("socket.io")(Http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 var position = {
   x: 200,
   y: 200,
 };
 
-Socketio.on("connectio", (socket) => {
+Socketio.on("connection", (socket) => {
   socket.emit("position", position);
+  socket.on("move", (data) => {
+    switch (data) {
+      case "left":
+        position.x -= 5;
+        Socketio.emit("position", position);
+        break;
+      case "right":
+        position.x += 5;
+        Socketio.emit("position", position);
+        break;
+      case "up":
+        position.y -= 5;
+        Socketio.emit("position", position);
+        break;
+      case "down":
+        position.x += 5;
+        Socketio.emit("position", position);
+        break;
+    }
+  });
 });
 
 Http.listen(3000, () => {
