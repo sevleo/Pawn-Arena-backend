83ccb6709370ee275cfc1ef030df272d55622709
Seva <seva.leonov@hotmail.com>
2024-07-16 12:10:32 +0700

    Add websocket implementation

--- app(ws).js
+++ app(ws).js
@@ -1,11 +1,11 @@
 const cors = require("cors");
 const express = require("express");
 const http = require("http");
-// const { Server } = require("socket.io");
 const WebSocket = require("ws");
 
 const app = express();
 const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 const corsOptions = {
   origin: "*",
@@ -16,13 +16,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-var position = {
+let position = {
   x: 200,
   y: 200,
 };
-
-// Websockets implementation
-const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => {
   console.log("a user connected");
@@ -61,35 +58,6 @@ wss.on("connection", (ws) => {
     console.log("user disconnected");
   });
 });
-
-// Socket.io implementation
-// const io = new Server(server, {
-//   cors: corsOptions,
-// });
-
-// io.on("connection", (socket) => {
-//   socket.emit("position", position);
-//   socket.on("move", (data) => {
-//     switch (data) {
-//       case "left":
-//         position.x -= 5;
-//         io.emit("position", position);
-//         break;
-//       case "right":
-//         position.x += 5;
-//         io.emit("position", position);
-//         break;
-//       case "up":
-//         position.y -= 5;
-//         io.emit("position", position);
-//         break;
-//       case "down":
-//         position.y += 5;
-//         io.emit("position", position);
-//         break;
-//     }
-//   });
-// });
 
 server.listen(3000, () => {
   console.log("Listening at :3000...");
