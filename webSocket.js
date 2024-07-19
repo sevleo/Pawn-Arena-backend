06b75e6b1c9ef6b1a132c0ec71f932f24df0bda2
Seva <seva.leonov@hotmail.com>
2024-07-19 16:54:49 +0700

    Refactor websocket connections

--- webSocket.js
+++ webSocket.js
@@ -5,16 +5,43 @@ function setupWebSocket(server) {
 
   let nextClientId = 0; // Initialize a counter for client IDs
   const clients = new Map();
+  let allPositions = [];
 
   wss.on("connection", (ws) => {
+    // Assign clientId and clientData to new connection
     const clientId = nextClientId++;
     const clientData = { ws, position: { x: 200, y: 200 } };
+
+    // Add new connection to clients map
     clients.set(clientId, clientData); // Store the WebSocket connection with its ID
     console.log("a user connected");
     console.log(clients);
 
-    // Send the current position to the newly connected client
-    ws.send(JSON.stringify({ type: "position", data: clientData.position }));
+    // Update allPositions array from clients map
+    allPositions = Array.from(clients.entries()).map(([id, data]) => ({
+      clientId: id,
+      position: data.position,
+    }));
+    console.log(allPositions);
+
+    // Send the initial position of the new connection to the client
+    ws.send(
+      JSON.stringify({
+        type: "initial position",
+        data: {
+          position: clientData.position,
+          clientId: clientId,
+          allPositions: allPositions,
+        },
+      })
+    );
+
+    // Broadcast the updated position to all clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: "position", data: allPositions }));
+      }
+    });
 
     ws.on("message", (message) => {
       const msg = JSON.parse(message);
@@ -36,14 +63,11 @@ function setupWebSocket(server) {
           }
         }
 
-        // Broadcast all clients' positions
-        const allPositions = Array.from(clients.entries()).map(
-          ([id, data]) => ({
-            clientId: id,
-            position: data.position,
-          })
-        );
-
+        // Update allPositions array from clients map
+        allPositions = Array.from(clients.entries()).map(([id, data]) => ({
+          clientId: id,
+          position: data.position,
+        }));
         console.log(allPositions);
 
         // Broadcast the updated position to all clients
