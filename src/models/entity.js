efdf9ce20c1ae4ab9d906bc8c7296af925ced3d6
Seva <seva.leonov@hotmail.com>
2024-08-22 12:14:41 +0700

    Add diagonal factor

--- src/models/entity.js
+++ src/models/entity.js
@@ -13,19 +13,25 @@ class Entity {
   }
 
   applyInput(input) {
-    switch (input.direction) {
-      case "right":
-        this.position.x += input.press_time * this.speed;
-        break;
-      case "left":
-        this.position.x -= input.press_time * this.speed;
-        break;
-      case "up":
-        this.position.y -= input.press_time * this.speed;
-        break;
-      case "down":
-        this.position.y += input.press_time * this.speed;
-        break;
+    let xForce = 0;
+    let yForce = 0;
+
+    if (input.active_keys.right) xForce = this.speed * input.press_time;
+    if (input.active_keys.left) xForce = -this.speed * input.press_time;
+    if (input.active_keys.up) yForce = -this.speed * input.press_time;
+    if (input.active_keys.down) yForce = this.speed * input.press_time;
+
+    if (xForce !== 0 || yForce !== 0) {
+      const diagonalFactor = 0.7071; // Approximation of 1/√2 for 45-degree movement
+
+      if (xForce !== 0 && yForce !== 0) {
+        xForce *= diagonalFactor;
+        yForce *= diagonalFactor;
+      }
+
+      // Update the position of the entity
+      this.position.x += xForce;
+      this.position.y += yForce;
     }
   }
 }
