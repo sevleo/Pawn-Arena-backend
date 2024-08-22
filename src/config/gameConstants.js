41b85f48089e68cdd9f34680380d5f6db2981460
Seva <seva.leonov@hotmail.com>
2024-08-22 19:03:37 +0700

    Update

--- src/config/gameConstants.js
+++ src/config/gameConstants.js
@@ -6,7 +6,7 @@ const CANVAS_HEIGHT = 800;
 const GAME_SPEED_RATE = 16.67;
 
 // Websocket connection
-const BROADCAST_RATE_INTERVAL = 200;
+const BROADCAST_RATE_INTERVAL = 16.67;
 
 // Pawn
 const RADIUS = 10;
