6ccd356743fb9c2028e46cd3a2d78301899c466c
Seva <seva.leonov@hotmail.com>
2024-08-23 17:03:32 +0700

    Update

--- src/models/entity.js
+++ src/models/entity.js
@@ -14,9 +14,9 @@ class Entity {
       label: "entity",
       clientId: clientId,
       isStatic: false,
-      restitution: 0,
+      restitution: 0.5,
       friction: 0.5,
-      frictionAir: 0.5,
+      frictionAir: 0.01,
     });
 
     Composite.add(world, this.entityBody);
